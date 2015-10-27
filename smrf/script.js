e0579ebb5063808dca7ac206ed817c931539618b
Mohab <mohab.alsamman@moby-hq.com>
2015-10-27 22:25:40 +0400

    Added send email, and card view

--- smrf/script.js
+++ smrf/script.js
@@ -1,4 +1,6 @@
 (function ($) {
+	// +++++++++
+	// + configs
 	var configs = {
 		'emptyStrReplacement' : '__'
 	}
@@ -8,10 +10,160 @@
 	    autoclose: true,
 		todayHighlight: true
 	}
-	$(document).ready(function () {
-		$('.datepicker').datepicker(datepickerOpts);
-	});
+	// - configs
+	// ---------
 
+	// ++++++++++++
+	// + properties
+	var dataMap = {};
+	// - properties
+	// ------------
+
+	// +++++++++
+	// + helpers
+	function queryStrToObj(queryStr) {
+		params = {}
+		queryStr.substring(0, queryStr.length).split('&').forEach(function(val) {
+			params[val.substring(0, val.indexOf('='))] = decodeURI(val.substring(val.indexOf('=')+1, val.length)).replace(/\+/g, ' ').replace(/\n/g, '<br>');
+		});
+		return params;
+	}
+	// - helpers
+	// ---------
+
+	// ++++++++++++++++++++++
+	// + Templates generators
+	function repropagateCheckboxes(type) {
+		templatesVars[type].forEach(function(val) {
+			templatesVars[type] = $.grep(templatesVars[type], function(v) {
+				return v != val;
+			});
+			$('input#'+val).prop('checked', true).trigger('change');
+		});
+	}
+	var templatesVars = {
+		channel: '',
+		langs: [],
+		socialmedia: [],
+		imagetypes: []
+	};
+	function tempChannels(channels) {
+		var temp = _.template($('#channels_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+		$('#channels').html(temp({channels: channels}));
+		$('#channels').show();
+		$('#channels select').change(function() {
+			templatesVars.channel = $('#channels option:selected').val();
+			if (templatesVars.channel.length) {
+				tempLangs(dataMap.channels[templatesVars.channel].langs);
+				tempSocialmedia(dataMap.channels[templatesVars.channel].socialmedia);
+			} else {
+				tempLangs([]);
+				tempSocialmedia([]);
+			}
+		});
+	}
+	function tempLangs(langs) {
+		if (langs.length == 0) {
+			$('#langs input').prop('checked', false).trigger('change');
+			$('#langs').hide().html('');
+			return false;
+		}
+		_languages = []
+		langs.forEach(function (lang) {
+			_languages.push([lang, dataMap.langs[lang]]);
+		});
+		var temp = _.template($('#langs_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+		$('#langs').html(temp({langs: _languages}));
+		$('#langs').show();
+		$('#langs input').change(function() {
+			if (this.checked) {
+				templatesVars.langs.push(this.id);
+			} else {
+				lang = this.id;
+				templatesVars.langs = $.grep(templatesVars.langs, function(val) {return val != lang;});
+			}
+		});
+		repropagateCheckboxes('langs');
+	}
+	function tempSocialmedia(socialmedia) {
+		if (socialmedia.length == 0) {
+			$('#socialmedia input').prop('checked', false).trigger('change');
+			$('#socialmedia').hide().html('');
+			return false;
+		}
+
+		_socialmedia = [];
+		socialmedia.forEach(function(sm) {
+			_socialmedia.push([sm, dataMap.socialmedia[sm].title]);
+		});
+		var temp = _.template($('#socialmedia_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+		$('#socialmedia').html(temp({socialmedia: _socialmedia}));
+		$('#socialmedia').show();
+		$('#socialmedia input').change(function() {
+			if (this.checked) {
+				templatesVars.socialmedia.push(this.id);
+			} else {
+				socialmedia = this.id;
+				templatesVars.socialmedia = $.grep(templatesVars.socialmedia, function (val) {return val != socialmedia});
+			}
+			imagetypes = [];
+			templatesVars.socialmedia.forEach(function (sm) {
+				imagetypes.push.apply(imagetypes, Object.keys(dataMap.socialmedia[sm].imagetypes))
+			});
+			tempImagetypes($.grep(imagetypes, function(el, index) {
+				return index == $.inArray(el, imagetypes);
+			}));
+		});
+		repropagateCheckboxes('socialmedia');
+	}
+	function tempImagetypes(imagetypes) {
+		if (imagetypes.length == 0) {
+			$('#imagetype input').prop('checked', false).trigger('change');
+			$('#imagetype').hide().html('');
+			return false;
+		}
+		_imagetypes = [];
+		imagetypes.forEach(function(it) {
+			_imagetypes.push([it, dataMap.imagetypes[it].title]);
+		});
+		var temp = _.template($('#imagetypes_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+		$('#imagetype').html(temp({imagetypes: _imagetypes}));
+		$('#imagetype').show();
+		$('#imagetype input').change(function() {
+			if (this.checked) {
+				templatesVars.imagetypes.push(this.id);
+			} else {
+				imagetype = this.id;
+				templatesVars.imagetypes = $.grep(templatesVars.imagetypes, function(val) {return val != imagetype});
+			}
+		});
+		repropagateCheckboxes('imagetypes');
+	}
+	$(document).delegate('.requirements input, .requirements select', 'change', function() {
+		$('button#gencards').prop('disabled', !(Object.keys(templatesVars).every(function(key) {return templatesVars[key].length})));
+	});
+	function tempCard(cardData) {
+		if (cardData.length == 0) {
+			$('#cards').hide().html('');
+			$('#cardsfuncs').hide();
+			return false;
+		}
+		var temp = _.template($('#card_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+		$('#cards').append(temp({data: cardData}));
+		$('#cards').show();
+		$('.datepicker').datepicker(datepickerOpts);
+		$('#cardsfuncs').show();
+	}
+	function tempCardview(cardData) {
+		var temp = _.template($('#cardview_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+		$('#cards').append(temp({data: cardData}));
+		$('#cards').show();
+	}
+	// - Template generators
+	// ---------------------
+
+	// ++++++++
+	// + events
 	$(document).delegate('.removecard', 'click', function() {
 		$(this).parents('.card').remove();
 		if ($('#cards .card').length == 0) {
@@ -48,151 +200,46 @@
 			eventCategory: 'Cards',
 			eventAction: 'Print'
 		});
-	})
-
-	// ++++++++++++++++++++++
-	// + Templates generators
-	function repropagateCheckboxes(type) {
-		templatesVars[type].forEach(function(val) {
-			templatesVars[type] = $.grep(templatesVars[type], function(v) {
-				return v != val;
-			});
-			$('input#'+val).prop('checked', true).trigger('change');
-		});
-	}
-	var templatesVars = {
-		channel: '',
-		langs: [],
-		socialmedia: [],
-		imagetypes: []
-	};
-	function tempChannels(channels) {
-		var temp = _.template($('#channels_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
-		$('#channels').html(temp({channels: channels}));
-		$('#channels').show();
-		$('#channels select').change(function() {
-			templatesVars.channel = $('#channels option:selected').val();
-			if (templatesVars.channel.length) {
-				tempLangs(channelsMap.channels[templatesVars.channel].langs);
-				tempSocialmedia(channelsMap.channels[templatesVars.channel].socialmedia);
-			} else {
-				tempLangs([]);
-				tempSocialmedia([]);
-			}
-		});
-	}
-	function tempLangs(langs) {
-		if (langs.length == 0) {
-			$('#langs input').prop('checked', false).trigger('change');
-			$('#langs').hide().html('');
-			return false;
-		}
-		_languages = []
-		langs.forEach(function (lang) {
-			_languages.push([lang, channelsMap.langs[lang]]);
-		});
-		var temp = _.template($('#langs_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
-		$('#langs').html(temp({langs: _languages}));
-		$('#langs').show();
-		$('#langs input').change(function() {
-			if (this.checked) {
-				templatesVars.langs.push(this.id);
-			} else {
-				lang = this.id;
-				templatesVars.langs = $.grep(templatesVars.langs, function(val) {return val != lang;});
-			}
-		});
-		repropagateCheckboxes('langs');
-	}
-	function tempSocialmedia(socialmedia) {
-		if (socialmedia.length == 0) {
-			$('#socialmedia input').prop('checked', false).trigger('change');
-			$('#socialmedia').hide().html('');
-			return false;
-		}
-
-		_socialmedia = [];
-		socialmedia.forEach(function(sm) {
-			_socialmedia.push([sm, channelsMap.socialmedia[sm].title]);
-		});
-		var temp = _.template($('#socialmedia_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
-		$('#socialmedia').html(temp({socialmedia: _socialmedia}));
-		$('#socialmedia').show();
-		$('#socialmedia input').change(function() {
-			if (this.checked) {
-				templatesVars.socialmedia.push(this.id);
-			} else {
-				socialmedia = this.id;
-				templatesVars.socialmedia = $.grep(templatesVars.socialmedia, function (val) {return val != socialmedia});
-			}
-			imagetypes = [];
-			templatesVars.socialmedia.forEach(function (sm) {
-				imagetypes.push.apply(imagetypes, Object.keys(channelsMap.socialmedia[sm].imagetypes))
-			});
-			tempImagetypes($.grep(imagetypes, function(el, index) {
-				return index == $.inArray(el, imagetypes);
-			}));
-		});
-		repropagateCheckboxes('socialmedia');
-	}
-	function tempImagetypes(imagetypes) {
-		if (imagetypes.length == 0) {
-			$('#imagetype input').prop('checked', false).trigger('change');
-			$('#imagetype').hide().html('');
-			return false;
-		}
-		_imagetypes = [];
-		imagetypes.forEach(function(it) {
-			_imagetypes.push([it, channelsMap.imagetypes[it].title]);
-		});
-		var temp = _.template($('#imagetypes_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
-		$('#imagetype').html(temp({imagetypes: _imagetypes}));
-		$('#imagetype').show();
-		$('#imagetype input').change(function() {
-			if (this.checked) {
-				templatesVars.imagetypes.push(this.id);
-			} else {
-				imagetype = this.id;
-				templatesVars.imagetypes = $.grep(templatesVars.imagetypes, function(val) {return val != imagetype});
-			}
-		});
-		repropagateCheckboxes('imagetypes');
-	}
-	$(document).delegate('.requirements input, .requirements select', 'change', function() {
-		$('button#gencards').prop('disabled', !(Object.keys(templatesVars).every(function(key) {return templatesVars[key].length})));
 	});
-	function tempCard(cardData) {
-		if (cardData.length == 0) {
-			$('#cards').hide().html('');
-			$('#printcards').hide();
-			return false;
-		}
-		var temp = _.template($('#card_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
-		$('#cards').append(temp({data: cardData}));
-		$('#cards').show();
-		$('.datepicker').datepicker(datepickerOpts);
-		$('#printcards').show();
-
-		ga('send', {
-			hitType: 'event',
-			eventCategory: 'Cards',
-			eventAction: 'Generated',
-			eventLabel: JSON.stringify(cardData)
+	$(document).delegate('#emailcards', 'click', function() {
+		cards = [];
+		$('#cards .card form').each(function() {
+			cards.push(btoa($(this).serialize()));
 		});
-	}
-	// - Template generators
-	// ---------------------
+		temp = _.template($('#cardemail_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+		subject = 'Graphic Design Request';
+		body = temp({cards: cards});
+		window.location.href = 'mailto:?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
+	});
 
-	var channelsMap = {};
 	$(document).ready(function () {
+		// check for carddata param, if exists then it's a card view request
+		href = window.location.href
+		if (href.indexOf('?') > -1) {
+			qstr = queryStrToObj(href.substring(href.indexOf('?')+1, href.length));
+			if (qstr.hasOwnProperty('carddata')) {
+				cardData = queryStrToObj(atob(qstr.carddata));
+				console.log(cardData);
+				tempCardview(cardData);
+				return;
+			}
+		}
+
+		// if it wasn't card view request, show the form
+		$('main').show();
+
+		// load data map
 		$.ajax("map.json", {
 			"dataType": "json",
 			"success": function(map) {
 				// Building requirements form
-				channelsMap = map;
+				dataMap = map;
 				tempChannels(Object.keys(map.channels));
 			}
 		})
+
+		// initiate datepicter on date fields
+		$('.datepicker').datepicker(datepickerOpts);
 	});
 
 	$(document).delegate('form', 'submit', function(e) {
@@ -224,11 +271,11 @@
 		cardsData = [];
 		for (i1 = 0; i1 < data.channels.length; i1++) {
 			for (i2 = 0; i2 < data.langs.length; i2++) {
-				if (channelsMap.channels[data.channels[i1]].langs.indexOf(data.langs[i2]) == -1) continue;
+				if (dataMap.channels[data.channels[i1]].langs.indexOf(data.langs[i2]) == -1) continue;
 				for (i3 = 0; i3 < data.socialmedia.length; i3++) {
-					if (channelsMap.channels[data.channels[i1]].socialmedia.indexOf(data.socialmedia[i3]) == -1) continue;
+					if (dataMap.channels[data.channels[i1]].socialmedia.indexOf(data.socialmedia[i3]) == -1) continue;
 					for (i4 = 0; i4 < data.imagetypes.length; i4++) {
-						if (!channelsMap.socialmedia[data.socialmedia[i3]].imagetypes.hasOwnProperty(data.imagetypes[i4])) continue;
+						if (!dataMap.socialmedia[data.socialmedia[i3]].imagetypes.hasOwnProperty(data.imagetypes[i4])) continue;
 						cardsData.push({
 							'index' : cardsData.length,
 							'requestby': $.trim(data.requestby[0]).length == 0 ? configs.emptyStrReplacement : data.requestby[0],
@@ -236,11 +283,11 @@
 							'date': $.trim(data.date[0]).length == 0 ? configs.emptyStrReplacement : data.date[0],
 							'deadline': $.trim(data.deadline[0]).length == 0 ? configs.emptyStrReplacement: data.deadline[0],
 							'channel': data.channels[i1],
-							'lang': channelsMap.langs[data.langs[i2]],
+							'lang': dataMap.langs[data.langs[i2]],
 							'socialmedia_code': data.socialmedia[i3],
-							'socialmedia': channelsMap.socialmedia[data.socialmedia[i3]].title,
-							'imagetype': channelsMap.imagetypes[data.imagetypes[i4]].title,
-							'resolution': channelsMap.socialmedia[data.socialmedia[i3]].imagetypes[data.imagetypes[i4]]
+							'socialmedia': dataMap.socialmedia[data.socialmedia[i3]].title,
+							'imagetype': dataMap.imagetypes[data.imagetypes[i4]].title,
+							'resolution': dataMap.socialmedia[data.socialmedia[i3]].imagetypes[data.imagetypes[i4]]
 						});
 					}
 				}
@@ -249,7 +296,6 @@
 		cardsData.forEach(function(card) {
 			tempCard(card);
 		});
-		window.location.href = '#cards';
 
 		ga('send', {
 			hitType: 'event',
@@ -259,4 +305,7 @@
 			eventValue: cardsData.length
 		});
 	});
+	// - events
+	// --------
+
 }) (jQuery);
